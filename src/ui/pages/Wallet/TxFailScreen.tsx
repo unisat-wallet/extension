12e4a996e10b4cea045d8ee3882ae4813d7d9546
huanniangstudio <891863891@qq.com>
2023-03-04 10:33:50 +0800

    Fix text error

--- src/ui/pages/Wallet/TxFailScreen.tsx
+++ src/ui/pages/Wallet/TxFailScreen.tsx
@@ -18,8 +18,8 @@ export default function TxFailScreen() {
       <Content style={{ backgroundColor: '#1C1919' }}>
         <div className="flex flex-col items-center mx-auto mt-36 gap-2_5 w-110">
           <img src="./images/Delete.svg" alt="" />
-          <span className="mt-6 text-2xl">{t('Payment Faild')}</span>
-          <span className="text-error">{t('Your transaction had not succesfully sent')}</span>
+          <span className="mt-6 text-2xl">{t('Payment Failed')}</span>
+          <span className="text-error">{t('Your transaction has not succesfully sent')}</span>
         </div>
       </Content>
     </Layout>
