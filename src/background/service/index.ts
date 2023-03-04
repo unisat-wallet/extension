021bfd6bf50fc0c7d36ddd4ddee6f37cdd82d6bf
huanniangstudio <891863891@qq.com>
2023-03-04 11:38:00 +0800

    Add services

--- src/background/service/index.ts
+++ src/background/service/index.ts
@@ -3,3 +3,6 @@ export { default as i18n } from './i18n';
 export { default as keyringService } from './keyring';
 export { default as openapiService } from './openapi';
 export { default as preferenceService } from './preference';
+export { default as notificationService } from './notification';
+export { default as permissionService } from './permission';
+export { default as sessionService } from './session';
---
+++ src/background/service/session.ts
@@ -0,0 +1,81 @@
+import { permissionService } from '@/background/service';
+
+export class Session {
+  origin = '';
+
+  icon = '';
+
+  name = '';
+
+  constructor(data) {
+    if (data) {
+      this.setProp(data);
+    }
+  }
+
+  setProp({ origin, icon, name }) {
+    this.origin = origin;
+    this.icon = icon;
+    this.name = name;
+  }
+}
+
+// for each tab
+const sessionMap = new Map();
+
+const getSession = (id) => {
+  return sessionMap.get(id);
+};
+
+const getOrCreateSession = (id) => {
+  if (sessionMap.has(id)) {
+    return getSession(id);
+  }
+
+  return createSession(id, null);
+};
+
+const createSession = (id, data) => {
+  const session = new Session(data);
+  sessionMap.set(id, session);
+
+  return session;
+};
+
+const deleteSession = (id) => {
+  sessionMap.delete(id);
+};
+
+const broadcastEvent = (ev, data?, origin?) => {
+  let sessions: any[] = [];
+  sessionMap.forEach((session, key) => {
+    if (permissionService.hasPermission(session.origin)) {
+      sessions.push({
+        key,
+        ...session
+      });
+    }
+  });
+
+  // same origin
+  if (origin) {
+    sessions = sessions.filter((session) => session.origin === origin);
+  }
+
+  sessions.forEach((session) => {
+    try {
+      session.pushMessage?.(ev, data);
+    } catch (e) {
+      if (sessionMap.has(session.key)) {
+        deleteSession(session.key);
+      }
+    }
+  });
+};
+
+export default {
+  getSession,
+  getOrCreateSession,
+  deleteSession,
+  broadcastEvent
+};
