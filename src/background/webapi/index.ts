3fdee656127843ab8d156332796d9d936dd6568d
huanniangstudio <891863891@qq.com>
2023-03-04 11:15:52 +0800

    Add tab tools

--- src/background/webapi/index.ts
+++ src/background/webapi/index.ts
@@ -1,3 +1,4 @@
 export { default as notification } from './notification';
 export { default as storage } from './storage';
 export { default as winMgr } from './window';
+export { default as tab } from './tab';
