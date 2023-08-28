aa1863edd979a87effbd6deae2e4d5996ea83938
hunaniangstudio <891863891@qq.com>
2023-08-28 12:46:14 +0800

    chore: change api url

--- src/shared/constant/index.ts
+++ src/shared/constant/index.ts
@@ -227,8 +227,8 @@ export const INTERNAL_REQUEST_SESSION = {
   icon: './images/logo/logo@128x.png'
 };
 
-export const OPENAPI_URL_MAINNET = 'https://unisat.io/wallet-api-v4';
-export const OPENAPI_URL_TESTNET = 'https://unisat.io/testnet/wallet-api-v4';
+export const OPENAPI_URL_MAINNET = 'https://api.unisat.io/wallet-v4';
+export const OPENAPI_URL_TESTNET = 'https://api-testnet.unisat.io/wallet-v4';
 
 export const EVENTS = {
   broadcastToUI: 'broadcastToUI',
