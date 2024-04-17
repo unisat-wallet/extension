1d88ff3541a77bc014e14e81eb90f09ba00152bf
hunaniangstudio <891863891@qq.com>
2024-04-17 10:29:35 +0800

    chore: adjust the style of Arc20BalanceCard

--- src/ui/components/Arc20BalanceCard/index.tsx
+++ src/ui/components/Arc20BalanceCard/index.tsx
@@ -20,30 +20,19 @@ export default function Arc20BalanceCard(props: Arc2020BalanceCardProps) {
       style={{
         backgroundColor: '#141414',
         borderColor: 'rgba(255,255,255,0.1)',
-        borderWidth: 1,
-        height: 120,
-        minHeight: 120
+        borderWidth: 1
       }}
       fullX
       onClick={onClick}>
-      <Column full>
-        <Row justifyBetween itemsCenter>
-          <Text text={ticker} color="gold" />
-        </Row>
+      <Column full py="zero" gap="zero">
+        <Row fullY justifyBetween justifyCenter>
+          <Column fullY justifyCenter>
+            <Text text={ticker} color="gold" />
+          </Column>
 
-        <Row justifyBetween>
-          <Text text="Confirmed Balance:" color="textDim" size="xs" />
-          <Text text={confirmedBalance} size="xs" />
-        </Row>
-
-        <Row justifyBetween>
-          <Text text="Unconfirmed Balance:" color="textDim" size="xs" />
-          <Text text={unconfirmedBalance} size="xs" />
-        </Row>
-
-        <Row justifyBetween>
-          <Text text="Balance:" color="textDim" size="xs" />
-          <Text text={balance} size="xs" />
+          <Row itemsCenter fullY gap="zero">
+            <Text text={balance} size="xs" />
+          </Row>
         </Row>
       </Column>
     </Card>
