765f878c3af22779eafd7e772ab2c3049c274792
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-07-28 17:59:39 -0400

    fixes

--- src/ui/pages/OpNet/UnWrapBitcoinOpnet.tsx
+++ src/ui/pages/OpNet/UnWrapBitcoinOpnet.tsx
@@ -1,4 +1,4 @@
-import { IWBTCContract, WBTC_ABI, getContract } from 'opnet';
+import { getContract, IWBTCContract, WBTC_ABI } from 'opnet';
 import { useEffect, useMemo, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
@@ -12,12 +12,7 @@ import { OutputValueBar } from '@/ui/components/OutputValueBar';
 import { RBFBar } from '@/ui/components/RBFBar';
 import { useCurrentAccount } from '@/ui/state/accounts/hooks';
 import { useCurrentKeyring } from '@/ui/state/keyrings/hooks';
-import {
-  useFetchAssetUtxosRunesCallback,
-  useFetchUtxosCallback,
-  usePrepareSendRunesCallback,
-  useRunesTx
-} from '@/ui/state/transactions/hooks';
+import { useFetchUtxosCallback, useRunesTx } from '@/ui/state/transactions/hooks';
 import { colors } from '@/ui/theme/colors';
 import { fontSizes } from '@/ui/theme/font';
 import { useWallet } from '@/ui/utils';
@@ -70,7 +65,7 @@ export default function UnWrapBitcoinOpnet() {
 
   const fetchUtxos = useFetchUtxosCallback();
 
-  const fetchAssetUtxosRunes = useFetchAssetUtxosRunesCallback();
+  //const fetchAssetUtxosRunes = useFetchAssetUtxosRunesCallback();
   const tools = useTools();
   useEffect(() => {
     fetchUtxos();
@@ -98,7 +93,7 @@ export default function UnWrapBitcoinOpnet() {
     checkAvailableBalance();
   }, []);
 
-  const prepareSendRunes = usePrepareSendRunesCallback();
+  //const prepareSendRunes = usePrepareSendRunesCallback();
 
   const [feeRate, setFeeRate] = useState(5);
   const [enableRBF, setEnableRBF] = useState(false);
