b1ef814e0ba230ea5389e4ed40971307713b9a3f
AbangZ <413318038@qq.com>
2024-09-20 18:12:03 +0800

    update display for bool bridge tick.

--- src/ui/components/BRC20Ticker/index.tsx
+++ src/ui/components/BRC20Ticker/index.tsx
@@ -19,7 +19,21 @@ const $tickerPresets: { sm: { textSize: any }; md: { textSize: any }; lg: { text
 
 type Presets = keyof typeof $tickerPresets;
 
-export function BRC20Ticker({ tick, preset }: { tick: string | undefined; preset?: Presets }) {
+export const tickMod: { [key: string]: string } = {
+  'bBTC___': 'bBTC',
+  'bSATS_': 'bSATS',
+  'bORDI_': 'bORDI'
+};
+
+export function isBoolBridgeTick(tick: string) {
+  return !!tickMod[tick];
+}
+
+export function BRC20Ticker({ tick, preset, showOrigin=false }: {
+  tick: string | undefined,
+  preset?: Presets,
+  showOrigin?: boolean
+}) {
   const style = $tickerPresets[preset || 'md'];
   return useMemo(() => {
     if (!tick) return <></>;
@@ -35,6 +49,13 @@ export function BRC20Ticker({ tick, preset }: { tick: string | undefined; preset
         </Row>
       );
     }
-    return <Text text={tick} size={style.textSize} wrap color="ticker_color" />;
+    if(showOrigin && tickMod[tick]) {
+      return <Row gap="sm" itemsCenter>
+        <Text text={tickMod[tick] || tick} size={style.textSize} wrap color="ticker_color" />
+        <Text text={`(ticker: ${tick})`} size={style.textSize} wrap color="textDim" />
+      </Row>
+    }
+
+    return <Text text={tickMod[tick] || tick} size={style.textSize} wrap color="ticker_color" />;
   }, [tick]);
 }
