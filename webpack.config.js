6ec039cfd2cf81948aa4a17295131af852c693b4
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-09-19 22:41:52 -0400

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./build/webpack.common.config');
 const configs = {
-  dev: require('./build/webpack.dev.config'),
-  pro: require('./build/webpack.pro.config'),
-  debug: require('./build/webpack.debug.config')
+    dev: require('./build/webpack.dev.config'),
+    pro: require('./build/webpack.pro.config'),
+    debug: require('./build/webpack.debug.config')
 };
 
 const config = (env) => {
-  if (env.config == 'dev') {
-    process.env.NODE_ENV = 'development';
-    process.env.BABEL_ENV = 'development';
-  } else {
-    process.env.NODE_ENV = 'production';
-    process.env.BABEL_ENV = 'production';
-    process.env.TAILWIND_MODE = 'watch';
-  }
+    if (env.config == 'dev') {
+        process.env.NODE_ENV = 'development';
+        process.env.BABEL_ENV = 'development';
+    } else {
+        process.env.NODE_ENV = 'production';
+        process.env.BABEL_ENV = 'production';
+        process.env.TAILWIND_MODE = 'watch';
+    }
 
-  if (env.config) {
-    return webpackMerge.merge(commonConfig(env), configs[env.config]);
-  }
+    const toAdd = {
+        resolve: {
+            fallback: {
+                vm: require.resolve('vm-browserify')
+            }
+        }
+    };
 
-  return commonConfig(env);
+    if (env.config) {
+        return webpackMerge.merge(commonConfig(env), configs[env.config], toAdd);
+    }
+
+    return webpackMerge.merge(commonConfig(env), toAdd);
 };
 
 module.exports = config;
