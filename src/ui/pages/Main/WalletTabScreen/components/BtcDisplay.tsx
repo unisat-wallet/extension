fa7b138a420c7613a5a9b5ac67f41155444402f7
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-09-24 18:08:24 -0400

    Update BtcDisplay.tsx

--- src/ui/pages/Main/WalletTabScreen/components/BtcDisplay.tsx
+++ src/ui/pages/Main/WalletTabScreen/components/BtcDisplay.tsx
@@ -1,36 +1,40 @@
-import { Row, Text } from '@/ui/components';
-import { useBTCUnit, useChainType } from '@/ui/state/settings/hooks';
 import { useMemo } from 'react';
 
-export function BtcDisplay({ balance }: {
-  balance: string;
-}) {
-  const chainType = useChainType()
-  const btcUnit = useBTCUnit();
-  const {intPart,decPart} = useMemo(() => {
-    //   split balance into integer and decimal parts
-    const [intPart, decPart] = balance.split('.');
+import { Row, Text } from '@/ui/components';
+import { useBTCUnit, useChainType } from '@/ui/state/settings/hooks';
 
-    return {
-      intPart,
-      decPart: decPart || ''
-    };
+export function BtcDisplay({ balance }: { balance: string | number }) {
+    const chainType = useChainType();
+    const btcUnit = useBTCUnit();
+    const { intPart, decPart } = useMemo(() => {
+        //   split balance into integer and decimal parts
+        const [intPart, decPart] = balance.toString().split('.');
 
-  }, [balance]);
+        return {
+            intPart,
+            decPart: decPart || ''
+        };
+    }, [balance]);
 
-  if(chainType === 'FRACTAL_BITCOIN_MAINNET' || chainType === 'FRACTAL_BITCOIN_TESTNET'){
-  //   show 3 decimal places for fractal bitcoin
-    return <Row style={{alignItems:'flex-end'}} justifyCenter gap={'zero'}  my="sm">
-      <Text text={intPart} preset="title-bold"  size="xxxl" />
-      {
-        decPart && <Text text={'.' + decPart.slice(0, 3)} preset="title-bold" style={{
-          color:'#8a8a8a',
-          fontSize:28
-        }} />
-      }
-      <Text text={btcUnit} preset="title-bold" size="xxxl" style={{marginLeft:'0.25em'}}  />
-    </Row>;
-  }
+    if (chainType === 'FRACTAL_BITCOIN_MAINNET' || chainType === 'FRACTAL_BITCOIN_TESTNET') {
+        //   show 3 decimal places for fractal bitcoin
+        return (
+            <Row style={{ alignItems: 'flex-end' }} justifyCenter gap={'zero'} my="sm">
+                <Text text={intPart} preset="title-bold" size="xxxl" />
+                {decPart && (
+                    <Text
+                        text={'.' + decPart.slice(0, 3)}
+                        preset="title-bold"
+                        style={{
+                            color: '#8a8a8a',
+                            fontSize: 28
+                        }}
+                    />
+                )}
+                <Text text={btcUnit} preset="title-bold" size="xxxl" style={{ marginLeft: '0.25em' }} />
+            </Row>
+        );
+    }
 
-  return <Text text={balance + ' ' + btcUnit} preset="title-bold" textCenter size="xxxl" my="sm" />;
+    return <Text text={balance + ' ' + btcUnit} preset="title-bold" textCenter size="xxxl" my="sm" />;
 }
