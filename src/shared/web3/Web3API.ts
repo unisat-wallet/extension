19058ebb6b80b2f5412d727e5196fff4d5624ba4
Predmet <175954199+impredmet@users.noreply.github.com>
2024-09-27 02:14:46 +0200

    Update Web3API.ts

--- src/shared/web3/Web3API.ts
+++ src/shared/web3/Web3API.ts
@@ -22,20 +22,6 @@ import {
 
 BigNumber.config({ EXPONENTIAL_AT: 256 });
 
-export function shuffle<T>(array: T[]): void {
-    let currentIndex = array.length;
-
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-        // Pick a remaining element...
-        const randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
-}
-
 export function getOPNetChainType(chain: ChainType): ChainId {
     switch (chain) {
         case ChainType.FRACTAL_BITCOIN_MAINNET: {
