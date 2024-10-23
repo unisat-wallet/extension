5fb29c5b0d6e2bbba8b470ec42d2935959d5f62c
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-10-23 01:42:40 -0400

    Update index.tsx

--- src/ui/components/NavTabBar/index.tsx
+++ src/ui/components/NavTabBar/index.tsx
@@ -10,7 +10,7 @@ import { Icon, IconTypes } from '../Icon';
 
 export function NavTabBar({ tab }: { tab: TabOption }) {
     return (
-        <Grid columns={3} style={{ width: '100%', height: '67.5px', backgroundColor: colors.bg2 }}>
+        <Grid columns={2} style={{ width: '100%', height: '67.5px', backgroundColor: colors.bg2 }}>
             <TabButton tabName="home" icon="wallet" isActive={tab === 'home'} />
             <TabButton tabName="settings" icon="settings" isActive={tab === 'settings'} />
         </Grid>
