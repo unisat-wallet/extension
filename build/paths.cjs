48c454cce3f6dcdd6cfc8cfe23038f3d31e023b9
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-12-31 23:41:16 -0500

    fixes (broken install)

--- build/paths.cjs
+++ build/paths.cjs
@@ -5,33 +5,32 @@ const rootResolve = path.resolve.bind(path, appRoot);
 const buildPath = 'dist';
 
 function getBrowserPaths(browser) {
-  let ret = {
-    root: appRoot,
-    src: rootResolve('src'),
-    indexHtml: rootResolve('build/_raw/index.html'),
-    notificationHtml: rootResolve('build/_raw/notification.html'),
-    backgroundHtml: rootResolve('src/background/background.html'),
-    dist: rootResolve('dist/' + browser),
-    rootResolve,
-    dotenv: rootResolve('.env'),
-    appPath: rootResolve('.'),
-    appBuild: rootResolve(buildPath),
-    appPublic: rootResolve('public'),
-    appHtml: rootResolve('public/index.html'),
-    appPackageJson: rootResolve('package.json'),
-    appSrc: rootResolve('src'),
-    appTsConfig: rootResolve('tsconfig.json'),
-    appJsConfig: rootResolve('jsconfig.json'),
-    yarnLockFile: rootResolve('yarn.lock'),
-    proxySetup: rootResolve('src/setupProxy.js'),
-    appNodeModules: rootResolve('node_modules'),
-    appWebpackCache: rootResolve('node_modules/.cache'),
-    appTsBuildInfoFile: rootResolve('node_modules/.cache/tsconfig.tsbuildinfo')
-    // publicUrlOrPath,
-  };
-  return ret;
+    return {
+        root: appRoot,
+        src: rootResolve('src'),
+        indexHtml: rootResolve('build/_raw/index.html'),
+        notificationHtml: rootResolve('build/_raw/notification.html'),
+        backgroundHtml: rootResolve('src/background/background.html'),
+        dist: rootResolve('dist/' + browser),
+        rootResolve,
+        dotenv: rootResolve('.env'),
+        appPath: rootResolve('.'),
+        appBuild: rootResolve(buildPath),
+        appPublic: rootResolve('public'),
+        appHtml: rootResolve('public/index.html'),
+        appPackageJson: rootResolve('package.json'),
+        appSrc: rootResolve('src'),
+        appTsConfig: rootResolve('tsconfig.json'),
+        appJsConfig: rootResolve('jsconfig.json'),
+        yarnLockFile: rootResolve('yarn.lock'),
+        proxySetup: rootResolve('src/setupProxy.js'),
+        appNodeModules: rootResolve('node_modules'),
+        appWebpackCache: rootResolve('node_modules/.cache'),
+        appTsBuildInfoFile: rootResolve('node_modules/.cache/tsconfig.tsbuildinfo')
+        // publicUrlOrPath,
+    };
 }
 
 module.exports = {
-  getBrowserPaths
+    getBrowserPaths
 };
