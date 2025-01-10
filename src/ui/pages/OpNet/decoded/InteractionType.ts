f896fabbf5bb8ad341c2e35a977056ada740c3db
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-01-10 01:19:21 -0500

    Update InteractionType.ts

--- src/ui/pages/OpNet/decoded/InteractionType.ts
+++ src/ui/pages/OpNet/decoded/InteractionType.ts
@@ -1,28 +1,28 @@
 export enum InteractionOP20 {
-    Transfer = '27f576ca', // transfer(address,uint256)
-    TransferFrom = '23b872dd', // transferFrom(address,address,uint256)
-    Approve = '74e21680', // approve(address,uint256)
-    ApproveFrom = 'xxxxxx07', // approveFrom(address,uint256,bytes)
-    Burn = 'xxxxxx09', // burn(uint256)
-    Mint = 'xxxxxx10', // mint(address,uint256)
-    Airdrop = 'xxxxxx11', // airdrop(AddressMap<bigint>)
-    AirdropWithAmount = 'xxxxxx12' // airdropWithAmount(uint256,address[])
+    Transfer = '3b88ef57', // transfer(address,uint256)
+    TransferFrom = '4b6685e7', // transferFrom(address,address,uint256)
+    Approve = '9f0bb8a9', // approve(address,uint256)
+    ApproveFrom = '8e72bde0', // approveFrom(address,uint256,bytes)
+    Burn = '308dce5f', // burn(uint256)
+    Mint = '3950e061', // mint(address,uint256)
+    Airdrop = '3a546b21',
+    AirdropWithAmount = 'ca1a382d'
 }
 
 export enum InteractionMotoswap {
-    AddLiquidity = 'eb686505'
+    AddLiquidity = 'x'
 }
 
 export enum InteractionTypeNativeSwap {
-    Reserve = '6df24c37', // reserve(address,uint256,uint256,bool)
-    ListLiquidity = 'x', // listLiquidity(address,string,uint256,bool)
-    CancelListing = 'x', // cancelListing(address)
-    CreatePool = '6fae9005', // createPool(address,uint256,uint256,string,uint32,uint256,uint32)
-    CreatePoolWithSignature = 'x', // createPoolWithSignature(bytes,uint256,address,uint256,uint256,string,uint32,uint256,uint32)
-    SetFees = 'x', // setFees(uint256,uint256,uint256)
-    AddLiquidity = 'eb686505', // addLiquidity(address,string,uint256,bool)
-    RemoveLiquidity = 'x', // removeLiquidity(address,uint256)
-    Swap = 'da47c2f4' // swap(address)
+    Reserve = 'c63a50d7', // reserve(address,uint256,uint256,bool)
+    ListLiquidity = '2960f13b', // listLiquidity(address,string,uint256,bool)
+    CancelListing = 'a48e507c', // cancelListing(address)
+    CreatePool = 'ced27635', // createPool(address,uint256,uint256,string,uint32,uint256,uint32)
+    CreatePoolWithSignature = '4203f335', // createPoolWithSignature(bytes,uint256,address,uint256,uint256,string,uint32,uint256,uint32)
+    SetFees = 'b1a5f7c2', // setFees(uint256,uint256,uint256)
+    AddLiquidity = '90d83548', // addLiquidity(address,string,uint256,bool)
+    RemoveLiquidity = '70dccc7f', // removeLiquidity(address,uint256)
+    Swap = 'dbed39e2' // swap(address)
 }
 
 export type InteractionType = InteractionOP20 | InteractionTypeNativeSwap | InteractionMotoswap;
