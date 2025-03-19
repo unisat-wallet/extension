0eaaca3560c63b002e8011a675a55d91755e3280
Odyssey98 <yongbi1998@proton.me>
2025-03-19 18:59:38 +0800

    feat: only show More button on fractal chain

--- src/ui/pages/Main/WalletTabScreen/components/WalletActions/index.tsx
+++ src/ui/pages/Main/WalletTabScreen/components/WalletActions/index.tsx
@@ -24,6 +24,13 @@ export const WalletActions = ({ chain, address }: WalletActionsProps) => {
   const addressExplorerUrl = useAddressExplorerUrl(address);
   const [buyBtcModalVisible, setBuyBtcModalVisible] = useState(false);
 
+  const shouldUseMoreExpandedLayout = () => {
+    if (isFractal) return true;
+    // Maybe add more chains here in the future
+
+    return false;
+  };
+
   useEffect(() => {
     const checkUtxoClicked = async () => {
       const hasClickedUtxo = localStorage.getItem('utxo_clicked');
@@ -57,113 +64,156 @@ export const WalletActions = ({ chain, address }: WalletActionsProps) => {
     navigate('TxCreateScreen');
   };
 
+  const NewBadge = ({ top = -16, right = -10 }: { top?: number; right?: number }) => (
+    <div
+      style={{
+        position: 'absolute',
+        top,
+        right,
+        padding: '0px 5px',
+        borderRadius: 4,
+        backgroundColor: 'rgba(176, 37, 37, 0.25)',
+        zIndex: 10
+      }}>
+      <Text text="new!" color="red_light2" size="xxxs" />
+    </div>
+  );
+
   return (
     <>
-      <Row justifyCenter mt="md">
-        <Button text="Receive" preset="home" icon="receive" onClick={onReceiveClick} />
+      {!shouldUseMoreExpandedLayout() ? (
+        <Row justifyCenter mt="md">
+          <Button
+            text="Receive"
+            preset="home"
+            icon="receive"
+            onClick={(e) => {
+              navigate('ReceiveScreen');
+            }}
+          />
 
-        <Button text="Send" preset="home" icon="send" onClick={onSendClick} />
-        <Button text="History" preset="home" icon="history" onClick={onHistoryClick} />
-        {/* Custom div used to avoid Button component's style merging issues with toggle states */}
-        <div
-          style={{
-            display: 'flex',
-            minWidth: 64,
-            minHeight: 64,
-            flexDirection: 'column',
-            borderRadius: 16,
-            border: moreExpanded ? '1px solid rgba(244, 182, 44, 0.25)' : '1px solid #FFFFFF4D',
-            background: moreExpanded ? 'rgba(244, 182, 44, 0.10)' : '#2a2626',
-            padding: 5,
-            marginRight: 5,
-            marginLeft: 5,
-            justifyContent: 'center',
-            alignItems: 'center',
-            cursor: 'pointer',
-            position: 'relative'
-          }}
-          onClick={() => setMoreExpanded(!moreExpanded)}>
-          {!moreExpanded && (
-            <>
-              {!utxoClicked && (
-                <div
-                  style={{
-                    position: 'absolute',
-                    top: -16,
-                    right: -10,
-                    padding: '0px 5px',
-                    borderRadius: 4,
-                    backgroundColor: 'rgba(176, 37, 37, 0.25)',
-                    zIndex: 10
-                  }}>
-                  <Text text="new!" color="red_light2" size="xxxs" />
-                </div>
-              )}
-
-              <div
-                style={{
-                  position: 'absolute',
-                  top: -12,
-                  right: -12,
-                  zIndex: 5
-                }}>
-                <Icon icon="utxobg" size={32} />
-              </div>
-            </>
-          )}
-          <Icon icon="more" style={{ marginBottom: 7 }} />
-          <Text text="More" color="white" size="xs" />
-        </div>
-      </Row>
-
-      {moreExpanded && (
-        <Row justifyEnd mt="md">
-          <div style={{ position: 'relative' }}>
-            <Button
-              text="UTXO"
-              preset="home"
-              icon="utxo"
-              onClick={handleUtxoClick}
-              style={{
-                border: '1px solid rgba(244, 182, 44, 0.25)',
-                background: 'rgba(244, 182, 44, 0.10)'
-              }}
-            />
-            {!utxoClicked && (
-              <div
-                style={{
-                  position: 'absolute',
-                  top: -5,
-                  right: -5,
-                  padding: '0px 5px',
-                  borderRadius: 4,
-                  backgroundColor: 'rgba(176, 37, 37, 0.25)',
-                  zIndex: 10
-                }}>
-                <Text text="new!" color="red_light2" size="xxxs" />
-              </div>
-            )}
-          </div>
+          <Button
+            text="Send"
+            preset="home"
+            icon="send"
+            onClick={(e) => {
+              resetUiTxCreateScreen();
+              navigate('TxCreateScreen');
+            }}
+          />
+          <Button
+            text="History"
+            preset="home"
+            icon="history"
+            onClick={(e) => {
+              if (chain.isViewTxHistoryInternally) {
+                navigate('HistoryScreen');
+              } else {
+                window.open(addressExplorerUrl);
+              }
+            }}
+          />
           <Button
             text="Buy"
             preset="home"
-            icon={isFractal ? 'fb' : 'bitcoin'}
+            icon={chain.isFractal ? 'fb' : 'bitcoin'}
             iconSize={
-              isFractal
+              chain.isFractal
                 ? {
                     width: 24,
                     height: 11
                   }
                 : undefined
             }
-            onClick={() => setBuyBtcModalVisible(true)}
-            disabled={chainType !== ChainType.BITCOIN_MAINNET && chainType !== ChainType.FRACTAL_BITCOIN_MAINNET}
-            style={{
-              border: '1px solid rgba(244, 182, 44, 0.25)',
-              background: 'rgba(244, 182, 44, 0.10)'
+            onClick={(e) => {
+              setBuyBtcModalVisible(true);
             }}
+            disabled={chainType !== ChainType.BITCOIN_MAINNET && chainType !== ChainType.FRACTAL_BITCOIN_MAINNET}
           />
         </Row>
+      ) : (
+        <>
+          <Row justifyCenter mt="md">
+            <Button text="Receive" preset="home" icon="receive" onClick={onReceiveClick} />
+
+            <Button text="Send" preset="home" icon="send" onClick={onSendClick} />
+            <Button text="History" preset="home" icon="history" onClick={onHistoryClick} />
+            {/* Custom div used to avoid Button component's style merging issues with toggle states */}
+            <div
+              style={{
+                display: 'flex',
+                minWidth: 64,
+                minHeight: 64,
+                flexDirection: 'column',
+                borderRadius: 16,
+                border: moreExpanded ? '1px solid rgba(244, 182, 44, 0.25)' : '1px solid #FFFFFF4D',
+                background: moreExpanded ? 'rgba(244, 182, 44, 0.10)' : '#2a2626',
+                padding: 5,
+                marginRight: 5,
+                marginLeft: 5,
+                justifyContent: 'center',
+                alignItems: 'center',
+                cursor: 'pointer',
+                position: 'relative'
+              }}
+              onClick={() => setMoreExpanded(!moreExpanded)}>
+              {!moreExpanded && (
+                <>
+                  {!utxoClicked && <NewBadge />}
+
+                  <div
+                    style={{
+                      position: 'absolute',
+                      top: -12,
+                      right: -12,
+                      zIndex: 5
+                    }}>
+                    <Icon icon="utxobg" size={32} />
+                  </div>
+                </>
+              )}
+              <Icon icon="more" style={{ marginBottom: 7 }} />
+              <Text text="More" color="white" size="xs" />
+            </div>
+          </Row>
+
+          {moreExpanded && (
+            <Row justifyEnd mt="md">
+              <div style={{ position: 'relative' }}>
+                <Button
+                  text="UTXO"
+                  preset="home"
+                  icon="utxo"
+                  onClick={handleUtxoClick}
+                  style={{
+                    border: '1px solid rgba(244, 182, 44, 0.25)',
+                    background: 'rgba(244, 182, 44, 0.10)'
+                  }}
+                />
+                {!utxoClicked && <NewBadge top={-5} right={-5} />}
+              </div>
+              <Button
+                text="Buy"
+                preset="home"
+                icon={isFractal ? 'fb' : 'bitcoin'}
+                iconSize={
+                  isFractal
+                    ? {
+                        width: 24,
+                        height: 11
+                      }
+                    : undefined
+                }
+                onClick={() => setBuyBtcModalVisible(true)}
+                disabled={chainType !== ChainType.BITCOIN_MAINNET && chainType !== ChainType.FRACTAL_BITCOIN_MAINNET}
+                style={{
+                  border: '1px solid rgba(244, 182, 44, 0.25)',
+                  background: 'rgba(244, 182, 44, 0.10)'
+                }}
+              />
+            </Row>
+          )}
+        </>
       )}
 
       {buyBtcModalVisible && (
