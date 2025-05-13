428577172222a0612177b345428874ff0b553ec8
Odyssey <yongbi1998@proton.me>
2025-05-13 10:28:22 +0800

    style: AppDimensions

--- src/ui/components/Responsive.tsx
+++ src/ui/components/Responsive.tsx
@@ -1,13 +1,34 @@
+import { useMemo } from 'react';
+
 import { useExtensionIsInTab } from '../features/browser/tabs';
+import { getUiType } from '../utils';
 
 export const AppDimensions = (props) => {
   const extensionIsInTab = useExtensionIsInTab();
+  const isSidePanel = getUiType().isSidePanel;
+
+  const width = useMemo(() => {
+    if (extensionIsInTab) {
+      return '100vw';
+    }
+    return isSidePanel ? '100vw' : '357px';
+  }, [extensionIsInTab, isSidePanel]);
+
+  const height = useMemo(() => {
+    if (extensionIsInTab) {
+      return '100vh';
+    }
+    return isSidePanel ? '100vh' : '600px';
+  }, [extensionIsInTab, isSidePanel]);
 
   return (
     <div
       style={{
-        width: extensionIsInTab ? '100vw' : '357px',
-        height: extensionIsInTab ? '100vh' : '600px'
+        width,
+        height,
+        overscrollBehavior: 'contain',
+        overflowY: 'auto',
+        overflowX: 'hidden'
       }}
       {...props}
     />
